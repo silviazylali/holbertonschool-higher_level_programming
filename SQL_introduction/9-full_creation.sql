23be68415e15792cbb3ef15fad5f927a0527c57c
silviazylali <silviazylali0707@gmail.com>
2025-09-02 15:05:56 +0200

    Checked

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- script to create a database and a table if they do not exist, and insert values into the table
 
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table(id int, name varchar(256),score int);
+CREATE TABLE IF NOT EXISTS second_table(id int, name varchar(256),score int);
 
 INSERT INTO hbtn_0c_0.second_table (id,name,score)
 	VALUES(1,'John',10),(2,'Alex',3),(3,'Bob',14),(4,'George',8);
