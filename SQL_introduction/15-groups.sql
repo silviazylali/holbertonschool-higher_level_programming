2d4d3373c32c877ae5860629feda813efaffd3c0
silviazylali <silviazylali0707@gmail.com>
2025-09-02 14:51:44 +0200

    Checked

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- script to list the number of records with the same score in a table.
+
+
+
--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
 -- script to list all columns of a table
-SHOW COLUMNS FROM first_table;
+
+SHOW CREATE table FROM first_table;
