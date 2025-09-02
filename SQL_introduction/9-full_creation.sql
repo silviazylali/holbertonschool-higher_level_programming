21a1b6205d522c4feb175d223579dda7530c1b6d
silviazylali <silviazylali0707@gmail.com>
2025-09-02 15:07:43 +0200

    Checked

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,6 +2,6 @@
 
 CREATE TABLE IF NOT EXISTS second_table(id int, name varchar(256),score int);
 
-INSERT INTO hbtn_0c_0.second_table (id,name,score)
+INSERT INTO second_table (id,name,score)
 	VALUES(1,'John',10),(2,'Alex',3),(3,'Bob',14),(4,'George',8);
     
