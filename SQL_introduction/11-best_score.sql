888d24697aa2432fdec649a25836b8cb54c8da29
silviazylali <silviazylali0707@gmail.com>
2025-09-02 15:13:10 +0200

    Checked

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- script to list all values in a table with a score >= 10
 
-SELECT * FROM hbtn_0c_0.second_table WHERE score >= 10;
+SELECT score, name FROM hbtn_0c_0.second_table WHERE score >= 10;
