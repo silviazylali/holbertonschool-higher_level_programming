1feb307a2f43800b0b2c8fae7223ccae0fcf4b92
silviazylali <silviazylali0707@gmail.com>
2025-09-03 17:44:56 +0200

    Task 1

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- write a script that prints all privileges of user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- write a script that prints all privileges of user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
