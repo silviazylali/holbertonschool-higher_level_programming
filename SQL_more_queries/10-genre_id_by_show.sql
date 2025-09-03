aaa6710db7c5b9f2ed49fdc84df9d4e94c71e21a
silviazylali <silviazylali0707@gmail.com>
2025-09-03 18:26:11 +0200

    Task 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Query to list all TV shows with their genre IDs
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
