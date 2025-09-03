192745372bd20cf357fcb9a38356d33754b9e6a2
silviazylali <silviazylali0707@gmail.com>
2025-09-03 17:58:27 +0200

    Task 2

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- write a script that creates a user named user_0d_1 with password USER_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- write a script that grants all privileges to user_0d_1 on all databases and tables
+-- write a script that grants all privileges to user_0d_1 on the database hbtn_0d_1
 GRANT ALL PRIVILEGES ON *-* TO 'user_0d_1'@'localhost';
