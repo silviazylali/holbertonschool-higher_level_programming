d24341ca3cf127d27fa38c61bb9b09426d13e787
silviazylali <silviazylali0707@gmail.com>
2025-09-04 18:55:22 +0200

    Checked

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,3 +3,7 @@
 SELECT cities.id, cities.name FROM cities, states
 WHERE cities.state_id = states.id AND states.name = 'California'
 ORDER BY cities.id ASC;
+
+-- SELECT * FROM cities
+-- WHERE state_id = (select id from states where name = 'California')
+-- ORDER BY id ASC;
--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,11 @@
 -- Query to list all cities with their corresponding state names
 
+-- SELECT cities.id, cities.name, states.name
+-- FROM cities
+-- JOIN states ON cities.state_id = states.id
+-- ORDER BY cities.id ASC;
+
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = states.id
+INNER JOIN states ON states_id = cities.id
 ORDER BY cities.id ASC;
